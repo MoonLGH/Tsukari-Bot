8bfbbc52bc071aa5b0d213d0a51d705119bb2e5b
MoonLGH <MoonLMC1212@gmail.com>
2021-09-22 00:51:48 +0700

    yes

--- src/util/handler/EventHandler.ts
+++ src/util/handler/EventHandler.ts
@@ -4,7 +4,6 @@ export function setup(client: Client) {
 	const eventFiles = fs
 		.readdirSync("./src/Events")
 		.filter((file) => file.endsWith(".ts"));
-
 	for (const file of eventFiles) {
 		const event = require(`../../Events/${file}`);
 		if (event.once) {
