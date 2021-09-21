d6b8849e4c8e20a2df4aea50546ecc52d9fd65bd
MoonLGH <MoonLMC1212@gmail.com>
2021-09-22 00:56:54 +0700

    l

--- src/util/handler/EventHandler.ts
+++ src/util/handler/EventHandler.ts
@@ -2,8 +2,9 @@ import fs from "fs";
 import { Client } from "discord.js";
 export function setup(client: Client) {
 	const eventFiles = fs
-		.readdirSync("./src/Events")
+		.readdirSync("./Events")
 		.filter((file) => file.endsWith(".ts"));
+
 	for (const file of eventFiles) {
 		const event = require(`../../Events/${file}`);
 		if (event.once) {
