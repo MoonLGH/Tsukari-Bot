f9a360bd5206463eb0db51fe2a2bf4522b172804
MoonLGH <MoonLMC1212@gmail.com>
2021-09-22 00:43:46 +0700

    dont worry about this commit, i only forget to add intents

--- src/default.js
+++ src/default.js
@@ -7,7 +7,7 @@ module.exports ={
     boterror:"827236403263569980",
     defaultStatus: `To Your Messages | For Help Type ${dprefix}help`,
     ownerid: "460361291962515457", 
-    intents: ["GUILD_MESSAGES","GUILD_MESSAGE_REACTIONS","GUILDS","GUILD_PRESENCES","DIRECT_MESSAGES","DIRECT_MESSAGE_REACTIONS"],
+    intents: ["GUILD_MESSAGES","GUILD_MESSAGE_REACTIONS","GUILDS","GUILD_PRESENCES","DIRECT_MESSAGES","DIRECT_MESSAGE_REACTIONS","GUILD_MEMBERS"],
     partials:["CHANNEL"],
     devserver: "815213544218951740",
     token:"No"
